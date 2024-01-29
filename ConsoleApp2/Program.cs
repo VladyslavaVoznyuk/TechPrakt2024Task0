7b37e076acfb6b410291d715b2607cb20fc50b87
VladyslavaV <vt221_vvyu@student.ztu.edu.ua>
2024-01-29 18:19:43 +0200

    Консольний проєкт

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SumCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first numeric:");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second numeric:");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            double sum = number1 + number2;
+
+            Console.WriteLine($"Sum of num {number1} and {number2} = : {sum}");
+
+            Console.ReadLine(); 
+        }
+    }
+}
